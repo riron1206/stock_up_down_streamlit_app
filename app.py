d4f365ac045e22b8a2f03e57baa4c0235a9ea200
riron1206 <rironyokoi@gmail.com>
2021-06-28 00:20:38 +0900

    add py from my_PC

--- app.py
+++ app.py
@@ -288,4 +288,10 @@ def main():
 
 
 if __name__ == "__main__":
+    from matplotlib import font_manager
+
+    for i in font_manager.fontManager.ttflist:
+        if ".ttc" in i.fname:
+            print(i)
+
     main()
